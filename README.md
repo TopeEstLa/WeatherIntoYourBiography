08d2074f2f069b77f2df74baa03a06f39e8b535c
TopeEstLa <contact.tope95@gmail.com>
2021-10-14 20:48:52 +0200

    Push

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# WeatherIntoYourBiography
+# Welcome to WeatherIntoYourBiography 👋
+![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
+[![Twitter: TopeEstPasLa](https://img.shields.io/twitter/follow/TopeEstPasLa.svg?style=social)](https://twitter.com/TopeEstPasLa)
+
+> Add Weather into your instagram biography
+
+### 🏠 [Homepage](https://github.com/TopeEstLa/WeatherIntoYourBiography#readme)
+
+## Install
+
+```sh
+npm install
+cp config_example.json config.json
+```
+
+## Usage
+
+```sh
+node main.js
+```
+
+## Author
+
+👤 **TopeEstLa**
+
+* Website: TopeEstLa.me
+* Twitter: [@TopeEstPasLa](https://twitter.com/TopeEstPasLa)
+* Github: [@TopeEstLa](https://github.com/TopeEstLa)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/TopeEstLa/WeatherIntoYourBiography/issues). 
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ main.js
@@ -0,0 +1,60 @@
+const { USERNAME, PASSWORD, CITY, APPID } = require('./config.json');
+const { IgApiClient } = require('instagram-private-api');
+const hourToDayOrNight = require('hour-to-day-or-night');
+const openWeatherApi = require('openweather-apis');
+const cron = require('node-cron');
+
+const instagramClient = new IgApiClient();
+
+instagramClient.state.generateDevice(USERNAME);
+
+async function setupOpenWeatherApi() {
+    openWeatherApi.setLang('fr');
+    openWeatherApi.setCity(CITY);
+    openWeatherApi.setUnits('metric');
+    openWeatherApi.setAPPID(APPID);
+}
+
+async function loginInstagramClient() {
+    await instagramClient.simulate.preLoginFlow();
+    await instagramClient.account.login(USERNAME, PASSWORD);
+    process.nextTick(async () => await instagramClient.simulate.postLoginFlow());
+    //await instagramClient.account.setBiography(`It is currently ${new Date().toLocaleString()}`);
+}
+
+async function updateBiography(emoji) {
+    console.log("Biography updated");
+    await instagramClient.account.setBiography(emoji);
+}
+
+setupOpenWeatherApi();
+loginInstagramClient();
+
+console.log(`Started on ${new Date().toLocaleString()}`);
+
+cron.schedule('*/10 * * * *', async () => {
+
+    if (hourToDayOrNight(parseInt(new Date().getHours().toLocaleString())) === "day") {
+        openWeatherApi.getSmartJSON(async function (err, smart) {
+            switch (smart.description) {
+                case "ciel dégagé":
+                    await updateBiography("☀️");
+                    break;
+                case "peu nuageux":
+                    await updateBiography("🌤");
+                    break;
+                case "partiellement nuageux": case "couvert":
+                    await updateBiography("⛅️");
+                    break;
+                case "nuageux":
+                    await updateBiography("☁️");
+                    break;
+                default:
+                    console.log("Weather unrecognized " + smart.description);
+            }
+        });
+    } else {
+        await updateBiography("🌙");
+    }
+
+});
